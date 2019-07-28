ebe9593ee05dabe88f11c4db835459ca45f01d30
victor hugo <victor03016@gmail.com>
2019-07-28 14:00:32 +0200

    Consultando la API con useEffect
    Consultando la API con useEffect, he instalamos axios para las consultas a la api rest

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Frase from "./Components/Frase";
 
 function App() {
+    const [frase, getFrase] = useState({});
 
-  const [ frase, getFrase ] = useState({});
+    const consultaAPI = async () => {
+        const resultado = await axios(
+            "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+        );
+        //console.log(resultado.data[0]);
 
-  // Consulta a una rest api
+        // Añadimos el resultado de la api al state
+        getFrase(resultado.data[0]);
+    };
 
-  useEffect() => {
-    
-  }
+    // Consulta a una rest api
+    useEffect(() => {
+        consultaAPI();
+    }, []);
 
-
-  return (
-    <h1>Hola</h1>
-  );
-
+    return (
+        <div className="contenedor">
+            <Frase frase={frase} />
+            <button onClick={consultaAPI}> Nueva Frase</button>
+        </div>
+    );
 }
 
 export default App;
