336581fd8d9b9c227b7c9a3a5e166d50086ea4bc
victor hugo <victor03016@gmail.com>
2019-07-28 13:30:26 +0200

    Configuracion Inicial
    Añadimos los estilos y el logo de BreakingBad

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [ frase, getFrase ] = useState({});
+
+  // Consulta a una rest api
+
+  useEffect() => {
+    
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hola</h1>
   );
+
 }
 
 export default App;
